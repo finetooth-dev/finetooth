14433374071d8b01dc4fcfc545d839e6ad1ee3e3
omar-replit <95443937+omar-replit@users.noreply.github.com>
2024-07-09 18:14:21 +0000

    header style tweak

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-gray-400 text-gray-950">
       <body className="flex flex-col sans-serif min-h-dvh overscroll-none">
-        <header className="flex p-2 text-lg justify-between">
+        <header className="flex px-3 py-2 justify-between">
           <Link
             href="/"
             className="self-center hover:underline focus:underline focus:outline-none"
