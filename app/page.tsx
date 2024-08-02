472da5f07fbead667b8a646e8c9d569307096755
omar-replit <95443937+omar-replit@users.noreply.github.com>
2024-08-02 19:01:18 +0000

    Repeated projects, style

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,19 @@ import GlassCard from "@/components/GlassCard";
 import { Carousel } from "@/components/InfiniteCarousel";
 import projects from "@/util/projects";
 
+const repeatedProjects = [...projects, ...projects, ...projects];
+
 export default function Home() {
   return (
     <main className="flex-1 flex flex-col">
       <Carousel>
-        {projects.map((project, index) => (
+        {repeatedProjects.map((project, index) => (
           <GlassCard href={project.slug} key={index} title={project.client}>
-            <img src={project.bgImgURL} alt={project.col1} className="min-w-full min-h-full object-cover" />
+            <img
+              src={project.bgImgURL}
+              alt={project.col1}
+              className="min-w-full min-h-full object-cover"
+            />
           </GlassCard>
         ))}
       </Carousel>
