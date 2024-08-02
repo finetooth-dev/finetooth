1b56b410e3a5752d93f5924e77d027026756d753
Finisher <finisher@Finishers-MacBook-Pro.local>
2024-08-02 10:56:18 -0700

    add autoprefixer for browser support, made some tweaks to header to fix cut off

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,30 +3,33 @@ import "./globals.css";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Finetooth",
+    title: "Finetooth",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  const currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
 
-  return (
-    <html lang="en" className="bg-gray-400 text-gray-950">
-      <body className="flex sans-serif min-h-svh overscroll-none">
-        <header className="flex flex-col px-3 py-2 justify-between">
-          <Link
-            href="/"
-            className="self-center hover:underline focus:underline focus:outline-none"
-          >
-            <h1>Finetooth</h1>
-          </Link>
-          <p>©{currentYear}</p>
-        </header>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="" style={{ backgroundColor: "#F9F9F9" }}>
+            <body className="flex sans-serif min-h-svh overscroll-none">
+                <header
+                    className="flex flex-col px-3 py-2 justify-between fixed h-full"
+                    style={{ color: "#7E7E7E" }}
+                >
+                    <Link
+                        href="/"
+                        className="self-center hover:underline focus:underline focus:outline-none"
+                    >
+                        <h1>finetooth</h1>
+                    </Link>
+                    <p>c {currentYear}</p>
+                </header>
+                {children}
+            </body>
+        </html>
+    );
 }
