fbff00982acf35290f500a866fe12b82c4ecb3fa
omar-replit <95443937+omar-replit@users.noreply.github.com>
2024-08-03 05:25:08 +0000

    Type corrections, mobile handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,43 +3,47 @@ import "./globals.css";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-    title: "Finetooth",
+  title: "Finetooth",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
-    return (
-        <html lang="en" className="" style={{ backgroundColor: "#F9F9F9" }}>
-            <body className="flex sans-serif min-h-svh overscroll-none">
-                <header
-                    className="flex flex-col px-3 py-2 justify-between fixed h-full"
-                    style={{ color: "#7E7E7E", zIndex: "1" }}
-                >
-                    <div className="flex flex-col gap-4 justify-start">
-                        <Link
-                            href="/"
-                            className="hover:underline focus:underline focus:outline-none cursor-pointer"
-                        >
-                            <h1>finetooth</h1>
-                        </Link>
-                        <div></div>
-                    </div>
-                    <p>c {currentYear}</p>
-                </header>
-                <div
-                    className="absolute flex flex-col top-0 right-0 px-3 py-2  h-full justify-between items-end"
-                    style={{ color: "#7E7E7E", zIndex: "1" }}
-                >
-                    <div>creative coding; web development</div>
-                    <div className="cursor-pointer">[ work with us ]</div>
-                </div>
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html
+      lang="en"
+      className="overscroll-none"
+      style={{ backgroundColor: "#F9F9F9" }}
+    >
+      <body className="flex sans-serif h-svh overscroll-none">
+        <header
+          className="flex flex-col px-3 py-2 justify-between fixed h-full"
+          style={{ color: "#7E7E7E", zIndex: "1" }}
+        >
+          <div className="flex flex-col gap-4 justify-start">
+            <Link
+              href="/"
+              className="hover:underline focus:underline focus:outline-none cursor-pointer"
+            >
+              <h1>finetooth</h1>
+            </Link>
+            <div></div>
+          </div>
+          <p>c {currentYear}</p>
+        </header>
+        <div
+          className="absolute flex flex-col top-0 right-0 px-3 py-2  h-full justify-between items-end"
+          style={{ color: "#7E7E7E", zIndex: "1" }}
+        >
+          <div>creative coding; web development</div>
+          <div className="cursor-pointer">[ work with us ]</div>
+        </div>
+        {children}
+      </body>
+    </html>
+  );
 }
